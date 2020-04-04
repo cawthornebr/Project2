d21fd77490d904e839c681a841c4710013aeea00
cawthornebr <56853166+cawthornebr@users.noreply.github.com>
2020-04-04 14:06:19 -0700

    update

--- app.py
+++ app.py
@@ -19,7 +19,6 @@ engine = create_engine("sqlite:///project1_db.db")
 Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
-db_323 = Base.classes.db323
 db_324 = Base.classes.db324
 db_325 = Base.classes.db325
 db_326 = Base.classes.db326
@@ -49,14 +48,26 @@ def index():
     # Create our session (link) from Python to the DB
     # session = Session(engine)
 
-    # Query all passengers
-    results = session.query(db_323.Admin2).filter(db_323.Province_State == "Arizona")
-    all_names = list(results)
-    all_names
+    # Query all confirmed cases, one for each day
+    master_list = []
+    master_list.append(list(session.query(db_324.Confirmed).filter(db_324.Province_State == "Arizona")))
+    master_list.append(list(session.query(db_325.Confirmed).filter(db_325.Province_State == "Arizona")))
+    master_list.append(list(session.query(db_326.Confirmed).filter(db_326.Province_State == "Arizona")))
+    master_list.append(list(session.query(db_327.Confirmed).filter(db_327.Province_State == "Arizona")))
+    master_list.append(list(session.query(db_328.Confirmed).filter(db_328.Province_State == "Arizona")))
+    master_list.append(list(session.query(db_329.Confirmed).filter(db_329.Province_State == "Arizona")))
+    master_list.append(list(session.query(db_330.Confirmed).filter(db_330.Province_State == "Arizona")))
+    master_list.append(list(session.query(db_331.Confirmed).filter(db_331.Province_State == "Arizona")))
+    master_list.append(list(session.query(db_401.Confirmed).filter(db_401.Province_State == "Arizona")))
+    master_list.append(list(session.query(db_402.Confirmed).filter(db_402.Province_State == "Arizona")))
+    master_list.append(list(session.query(db_403.Confirmed).filter(db_403.Province_State == "Arizona")))
+
+    county_list = list(session.query(db_403.Admin2).filter(db_403.Province_State == "Arizona"))
+    county_list
     # print (results)
     session.close()
 
-    return jsonify(all_names)
+    return jsonify(master_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
