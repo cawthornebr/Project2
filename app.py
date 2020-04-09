5d2ff4ad175b0cb07158d1d25ef0bc7f77da64e6
cawthornebr <56853166+cawthornebr@users.noreply.github.com>
2020-04-08 21:26:18 -0700

    updated app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from flask import Flask, jsonify, redirect, render_template
 from scipy import stats
 import numpy as np
 import pandas as pd
@@ -8,7 +9,6 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, inspect, func
-from flask import Flask, jsonify, redirect, render_template
 
 #################################################
 # Database Setup
@@ -30,6 +30,11 @@ db_331 = Base.classes.db331
 db_401 = Base.classes.db401
 db_402 = Base.classes.db402
 db_403 = Base.classes.db403
+db_404 = Base.classes.db404
+db_405 = Base.classes.db405
+db_406 = Base.classes.db406
+db_407 = Base.classes.db407
+db_408 = Base.classes.db408
 session = Session(engine)
 
 #################################################
@@ -42,33 +47,58 @@ app = Flask(__name__)
 #################################################
 
 #List of confirmed cases by date
-master_list = []
-master_list.append(list(session.query(db_324.Confirmed).filter(db_324.Province_State == "Arizona")))
-master_list.append(list(session.query(db_325.Confirmed).filter(db_325.Province_State == "Arizona")))
-master_list.append(list(session.query(db_326.Confirmed).filter(db_326.Province_State == "Arizona")))
-master_list.append(list(session.query(db_327.Confirmed).filter(db_327.Province_State == "Arizona")))
-master_list.append(list(session.query(db_328.Confirmed).filter(db_328.Province_State == "Arizona")))
-master_list.append(list(session.query(db_329.Confirmed).filter(db_329.Province_State == "Arizona")))
-master_list.append(list(session.query(db_330.Confirmed).filter(db_330.Province_State == "Arizona")))
-master_list.append(list(session.query(db_331.Confirmed).filter(db_331.Province_State == "Arizona")))
-master_list.append(list(session.query(db_401.Confirmed).filter(db_401.Province_State == "Arizona")))
-master_list.append(list(session.query(db_402.Confirmed).filter(db_402.Province_State == "Arizona")))
-master_list.append(list(session.query(db_403.Confirmed).filter(db_403.Province_State == "Arizona")))
+master_list_confirmed = []
+master_list_confirmed.append(list(session.query(db_324.Confirmed).filter(db_324.Province_State == "Arizona")))
+master_list_confirmed.append(list(session.query(db_325.Confirmed).filter(db_325.Province_State == "Arizona")))
+master_list_confirmed.append(list(session.query(db_326.Confirmed).filter(db_326.Province_State == "Arizona")))
+master_list_confirmed.append(list(session.query(db_327.Confirmed).filter(db_327.Province_State == "Arizona")))
+master_list_confirmed.append(list(session.query(db_328.Confirmed).filter(db_328.Province_State == "Arizona")))
+master_list_confirmed.append(list(session.query(db_329.Confirmed).filter(db_329.Province_State == "Arizona")))
+master_list_confirmed.append(list(session.query(db_330.Confirmed).filter(db_330.Province_State == "Arizona")))
+master_list_confirmed.append(list(session.query(db_331.Confirmed).filter(db_331.Province_State == "Arizona")))
+master_list_confirmed.append(list(session.query(db_401.Confirmed).filter(db_401.Province_State == "Arizona")))
+master_list_confirmed.append(list(session.query(db_402.Confirmed).filter(db_402.Province_State == "Arizona")))
+master_list_confirmed.append(list(session.query(db_403.Confirmed).filter(db_403.Province_State == "Arizona")))
+master_list_confirmed.append(list(session.query(db_404.Confirmed).filter(db_404.Province_State == "Arizona")))
+master_list_confirmed.append(list(session.query(db_405.Confirmed).filter(db_405.Province_State == "Arizona")))
+master_list_confirmed.append(list(session.query(db_406.Confirmed).filter(db_406.Province_State == "Arizona")))
+master_list_confirmed.append(list(session.query(db_407.Confirmed).filter(db_407.Province_State == "Arizona")))
+master_list_confirmed.append(list(session.query(db_408.Confirmed).filter(db_408.Province_State == "Arizona")))
+
+#List of death cases by date
+master_list_deaths = []
+master_list_deaths.append(list(session.query(db_324.Deaths).filter(db_324.Province_State == "Arizona")))
+master_list_deaths.append(list(session.query(db_325.Deaths).filter(db_325.Province_State == "Arizona")))
+master_list_deaths.append(list(session.query(db_326.Deaths).filter(db_326.Province_State == "Arizona")))
+master_list_deaths.append(list(session.query(db_327.Deaths).filter(db_327.Province_State == "Arizona")))
+master_list_deaths.append(list(session.query(db_328.Deaths).filter(db_328.Province_State == "Arizona")))
+master_list_deaths.append(list(session.query(db_329.Deaths).filter(db_329.Province_State == "Arizona")))
+master_list_deaths.append(list(session.query(db_330.Deaths).filter(db_330.Province_State == "Arizona")))
+master_list_deaths.append(list(session.query(db_331.Deaths).filter(db_331.Province_State == "Arizona")))
+master_list_deaths.append(list(session.query(db_401.Deaths).filter(db_401.Province_State == "Arizona")))
+master_list_deaths.append(list(session.query(db_402.Deaths).filter(db_402.Province_State == "Arizona")))
+master_list_deaths.append(list(session.query(db_403.Deaths).filter(db_403.Province_State == "Arizona")))
+master_list_deaths.append(list(session.query(db_404.Deaths).filter(db_404.Province_State == "Arizona")))
+master_list_deaths.append(list(session.query(db_405.Deaths).filter(db_405.Province_State == "Arizona")))
+master_list_deaths.append(list(session.query(db_406.Deaths).filter(db_406.Province_State == "Arizona")))
+master_list_deaths.append(list(session.query(db_407.Deaths).filter(db_407.Province_State == "Arizona")))
+master_list_deaths.append(list(session.query(db_408.Deaths).filter(db_408.Province_State == "Arizona")))
 
 #AZ county list - county_list_strip
 county_list = list(session.query(db_403.Admin2).filter(db_403.Province_State == "Arizona"))
-county_list_1 = []
+county_list1 = []
 for i in county_list:
-    county_list_1.append(str(i[-1]))
+    county_list1.append(str(i[-1]))
 
 #Close session
 session.close()
 
 #Date list
-date_list = ["3/24","3/25","3/26","3/27","3/28","3/29","3/30","3/31","4/1","4/2","4/3"]
+date_list = ["March_24","March_25","March_26","March_27","March_28","March_29","March_30","March_31","April_1",
+             "April_2","April_3","April_4","April_5","April_6","April_7","April_8"]
 
-#DataFrame setup - county_df
-x=[str(l).strip("(,)") for i in master_list for l in i]
+#DataFrame setup for confirmed cases
+x=[str(l).strip("(,)") for i in master_list_confirmed for l in i]
 n=16
 final = [x[i * n:(i + 1) * n] for i in range((len(x) + n - 1) // n )]
 dict = {}
@@ -76,12 +106,28 @@ count=0
 for i in final:
     dict.update({date_list[count]:i})
     count+=1
-county_df = pd.DataFrame(dict,index = county_list_1)
+county_confirmed_df = pd.DataFrame(dict,index = county_list1).drop(["Unassigned"])
 
-#JSONify the dataframe
-json_data_for_graphing1 = county_df.to_json(orient='columns')
-json_data_for_graphing2 = county_df.to_json(orient='index')
-json_data_for_graphing3 = county_df.to_json(orient='values')
+#DataFrame setup for death cases
+x=[str(l).strip("(,)") for i in master_list_deaths for l in i]
+n=16
+final = [x[i * n:(i + 1) * n] for i in range((len(x) + n - 1) // n )]
+dict = {}
+count=0
+for i in final:
+    dict.update({date_list[count]:i})
+    count+=1
+county_deaths_df = pd.DataFrame(dict,index = county_list1).drop(["Unassigned"])
+
+#JSONify the dataframe of confirmed cases
+json_confirmed_data_for_graphing1 = county_confirmed_df.to_json(orient='columns')
+json_confirmed_data_for_graphing2 = county_confirmed_df.to_json(orient='index')
+json_confirmed_data_for_graphing3 = county_confirmed_df.to_json(orient='values')
+
+#JSONify the dataframe of confirmed cases
+json_deaths_data_for_graphing1 = county_deaths_df.to_json(orient='columns')
+json_deaths_data_for_graphing2 = county_deaths_df.to_json(orient='index')
+json_deaths_data_for_graphing3 = county_deaths_df.to_json(orient='values')
 
 #################################################
 # Flask Routes
@@ -92,6 +138,6 @@ def index():
     print("----------------------")
     print("index")
     print("----------------------")
-
+    return(json_confirmed_data_for_graphing1)
 if __name__ == '__main__':
     app.run(debug=True)
